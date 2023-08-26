2a8efbdcaf937c3453c7b8aee902a50c9e39379a
Vishal Suman <vishal21suman@outlook.com>
2023-08-27 03:58:53 +0530

    completed

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+document
+  .getElementById("year")
+  .appendChild(document.createTextNode(new Date().getFullYear()))
+
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links")
+  const icon = document.querySelector(".hamburger-icon")
+  menu.classList.toggle("open")
+  icon.classList.toggle("open")
 }
